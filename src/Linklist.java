762d38ce839605993a126a3d6f1b6865630111dd
於涛 <yutao@yunjiglobal.com>
2020-11-24 17:41:36 +0800

    单链表的插入/删除基本操作

--- src/Linklist.java
+++ src/Linklist.java
@@ -1,6 +1,12 @@
 /**
- * @Author 於涛
+ * @Author Microtao
  * @Date 2020/11/20-17:16
+ * <p>
+ * Node 类用于描述节点的属性
+ * <p>
+ * LNode 类用于描述单链表的抽象：
+ * node ：代表单链表中的节点
+ * nextNode:保存的是下一个节点的地址
  */
 class Node {
     String name;
@@ -31,13 +37,13 @@ class LNode {
 public class Linklist {
 
     /**
-     * 插入链表数据
-     * */
+     * 插入数据到链表
+     */
     public LNode insert(LNode head, Node nodeData) {
         //LNode用于保存当前需要保存节点的信息，tempNode用于寻找整个链表
         LNode lNode, tempNode;
         if ((lNode = new LNode()) == null) {
-            System.out.println("创建节点失败");
+            System.out.println("内存空间已满，创建节点失败");
             return null;
         }
         //保存节点
@@ -49,15 +55,40 @@ public class Linklist {
         }
         tempNode = head;
         while (tempNode.nextNode != null) {
-             tempNode = tempNode.nextNode ;
+            tempNode = tempNode.nextNode;
         }
         tempNode.nextNode = lNode;
         return head;
     }
 
     /**
+     * 查找某个单链表中是否存在某个节点，如果存在删除它
+     *
+     * @param name 给定用户名
+     */
+    public boolean deleteNodeByName(LNode node, String name) {
+        LNode tempNode;
+        if (node == null) {
+            System.out.println("单链表为空，无法执行删除操作");
+            return false;
+        }
+        tempNode = node;
+        while (tempNode.nextNode != null) {
+            if (tempNode.nextNode.node.name.equals(name)) {
+                tempNode.nextNode = tempNode.nextNode.nextNode;
+                return true;
+            } else {
+                tempNode = tempNode.nextNode;
+            }
+        }
+        return true;
+    }
+
+    /**
      * 输出链表
-     * */
+     * <p>
+     * head节点中是没有保存数据的，所以需要找到head节点的nextNode节点才是实际存放数据的第一个节点
+     */
     public void printLinlist(LNode head) {
         while (head.nextNode != null) {
             System.out.println("name:" + head.nextNode.node.name + " age:" + head.nextNode.node.age);
@@ -69,14 +100,21 @@ public class Linklist {
     public static void main(String[] args) {
         LNode head = new LNode();
         Linklist linklist = new Linklist();
-        LNode node1 = linklist.insert(head, new Node("小明", 12));
-        LNode node2 = linklist.insert(node1, new Node("小强1", 13));
-        LNode node3 = linklist.insert(node2, new Node("小强2", 14));
-        LNode node4 = linklist.insert(node3, new Node("小强3", 15));
-        LNode node5 = linklist.insert(node4, new Node("小强4", 16));
-        LNode node6 = linklist.insert(node5, new Node("小强5", 17));
-        LNode node7 = linklist.insert(node6, new Node("小强6", 18));
-        linklist.printLinlist(node7);
+        //执行插入操作
+        linklist.insert(head, new Node("小强", 12));
+        linklist.insert(head, new Node("小强1", 13));
+        linklist.insert(head, new Node("小强2", 14));
+        linklist.insert(head, new Node("小强3", 15));
+        linklist.insert(head, new Node("小强4", 16));
+        linklist.insert(head, new Node("小强5", 17));
+        linklist.insert(head, new Node("小强6", 18));
+        System.out.println("-----------删除之前----------");
+        //输出整个链表
+        linklist.printLinlist(head);
+        //执行删除操作
+        linklist.deleteNodeByName(head, "小强2");
+        System.out.println("-----------删除之后----------");
+        linklist.printLinlist(head);
     }
 
 }
