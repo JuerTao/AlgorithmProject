79187c437947ff47161b564c5b35b59c626782ee
於涛 <yutao@yunjiglobal.com>
2020-11-26 15:05:03 +0800

    栈的操作

--- src/StackDemo.java
+++ src/StackDemo.java
@@ -1,10 +1,8 @@
-import com.sun.org.apache.xml.internal.resolver.helpers.PublicId;
-import com.sun.scenario.animation.AnimationPulseMBean;
+import java.util.Objects;
 
 /**
  * @Author 於涛
  * @Date 2020/11/24-18:06
- *
  * @Description :对栈执行增删改查操作
  */
 
@@ -27,17 +25,35 @@ class StackData {
                 ", age=" + age +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StackData stackData = (StackData) o;
+        return age == stackData.age &&
+                Objects.equals(name, stackData.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
 }
-class StackInfo{
-    public static int  MAXSIZE = 100;
+
+class StackInfo {
+    public static int MAXSIZE = 100;
     StackData[] data = new StackData[MAXSIZE];
-    //栈顶
+    //栈顶指针
     int top;
 }
+
 public class StackDemo {
-    /** 插入数据*/
-    public void insert(StackInfo stackInfo,StackData data){
-        if(stackInfo.top>=StackInfo.MAXSIZE){
+    /**
+     * 插入数据
+     */
+    public void insert(StackInfo stackInfo, StackData data) {
+        if (stackInfo.top >= StackInfo.MAXSIZE) {
             System.out.println("当前栈已满，无法插入！");
             return;
         }
@@ -46,32 +62,88 @@ public class StackDemo {
         return;
 
     }
-    /** 删除数据*/
-    public boolean delete(StackInfo stackInfo,StackData data){
+
+    /**
+     * 删除数据
+     */
+    public boolean delete(StackInfo stackInfo, StackData data) {
+        int pos = stackInfo.top;
+        if (stackInfo != null && stackInfo.top < 1) {
+            System.out.println("栈中的元素为空");
+            return false;
+        }
+        if (data == null) {
+            System.out.println("请指定需要修改的数据对象");
+            return false;
+        }
+       for(int i = pos;i>0;i--){
+           if (data.equals(stackInfo.data[i])) {
+               pos = i;
+              break;
+           }
+       }
+       for(int i = pos;i<stackInfo.top;i++){
+           stackInfo.data[i] = stackInfo.data[i+1];
+       }
+        --stackInfo.top;
         return true;
     }
-    /** 修改数据*/
-    public boolean update(StackInfo stackInfo,StackData data){
+
+    /**
+     * 修改数据
+     */
+    public boolean update(StackInfo stackInfo, StackData data) {
+
+        int pos = stackInfo.top;
+        if (stackInfo != null && stackInfo.top < 1) {
+            System.out.println("栈中的元素为空");
+            return false;
+        }
+        if (data == null) {
+            System.out.println("请指定需要修改的数据对象");
+            return false;
+        }
+        while (pos-- > 0) {
+            //姓名相同的可以认定为是同一个人,修改年龄
+            if (data.name.equals(stackInfo.data[pos].name)) {
+                stackInfo.data[pos].age = data.age;
+                return true;
+            }
+        }
         return true;
     }
-    /** 查找数据*/
-    public StackData getData(StackInfo stackInfo,StackData data){
+
+    /**
+     * 查找数据
+     */
+    public StackData getData(StackInfo stackInfo, StackData data) {
         return null;
     }
-    public void printf(StackInfo stackInfo){
-        if(stackInfo.top<=0){
+
+    public void printf(StackInfo stackInfo) {
+        int pos = stackInfo.top;
+        if (pos <= 0) {
             System.out.println("栈为空！");
         }
-        while(stackInfo.top-->0){
-            System.out.println(stackInfo.data[stackInfo.top]);
+        while (pos-- > 0) {
+            System.out.println(stackInfo.data[pos]);
         }
     }
+
     public static void main(String[] args) {
         StackInfo info = new StackInfo();
         StackDemo demo = new StackDemo();
-        for(int i = 0;i<10;i++){
-            demo.insert(info,new StackData("小明"+i,i));
+        System.out.println("-----------------执行插入操作-------------");
+        for (int i = 0; i < 5; i++) {
+            demo.insert(info, new StackData("小明" + i, i));
         }
+        //输出所有的栈元素
+        demo.printf(info);
+        System.out.println("-----------------执行修改操作-------------");
+        demo.update(info, new StackData("小明2", 58));
+        demo.printf(info);
+        System.out.println("-----------------执行删除操作-------------");
+        demo.delete(info, new StackData("小明2", 58));
         demo.printf(info);
     }
 }
